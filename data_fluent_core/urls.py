d0223eea55fbcd651696a8640ba5b7e0fdb120b1
10686142 <vmeerman.appdev@gmail.com>
2021-05-06 21:10:50 +0200

    fix: Moved SECRET_KEY out of settings.py to a gitnored file with a new key. Also named main app to data_fluent_core

--- data_fluent_core/urls.py
+++ data_fluent_core/urls.py
@@ -1,4 +1,4 @@
-"""upgrade_data_fluent URL Configuration
+"""data_fluent_core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
